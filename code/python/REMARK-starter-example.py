9ecefc338c6f95bf1f3f5f143d24a1e2def4e748
llorracc <Christopher.Carroll@jhu.edu>
2021-04-07 16:08:36 -0400

    Further fleshing out

--- code/python/REMARK-starter-example.py
+++ code/python/REMARK-starter-example.py
@@ -6,8 +6,8 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.4'
-#       jupytext_version: 1.2.3
+#       format_version: '1.5'
+#       jupytext_version: 1.10.2
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
@@ -21,29 +21,50 @@
 #     name: python
 #     nbconvert_exporter: python
 #     pygments_lexer: ipython3
-#     version: 3.7.6
+#     version: 3.8.8
+#   latex_envs:
+#     LaTeX_envs_menu_present: true
+#     autoclose: false
+#     autocomplete: true
+#     bibliofile: biblio.bib
+#     cite_by: apalike
+#     current_citInitial: 1
+#     eqLabelWithNumbers: true
+#     eqNumInitial: 1
+#     hotkeys:
+#       equation: Ctrl-E
+#       itemize: Ctrl-I
+#     labels_anchors: false
+#     latex_user_defs: false
+#     report_style_numbering: false
+#     user_envs_cfg: false
 # ---
 
+# +
+
 # ## A Sample Notebook in a Sample REMARK
-#
+# 
 # An example notebook which has some basic content which a REMARK may include.
-#
+# 
 # The REMARK notebook shouldn't just contain the code, use the markdown cells to explain the code and models.
+# -
 
 # required imports
 from HARK.ConsumptionSaving.ConsPrefShockModel import KinkyPrefConsumerType
 import numpy as np
 import matplotlib.pyplot as plt
-from custom_code import plot_distribution
+
 
 # Make and solve a preference shock consumer
 PrefShockExample = KinkyPrefConsumerType()
 PrefShockExample.cycles = 0  # Infinite horizon
 PrefShockExample.solve()
 
+
 # Unpack the consumption function for easier access.
 PrefShockExample.unpack('cFunc')
 PrefShockExample.unpack('mNrmMin')
+
 
 # Plot the consumption function at each discrete shock
 m = np.linspace(PrefShockExample.mNrmMin[0], 5, 200)
@@ -53,10 +74,16 @@ for j in range(PrefShockExample.PrefShkDstn[0].pmf.size):
     plt.plot(m, c)
 plt.xlim([-1.0, None])
 plt.ylim([0.0, None])
-plt.savefig('../figures/cFunc.png')
+plt.savefig('../../figures/cFunc.pdf')
+plt.ion() # Necessary so that when executed from the command line it does not wait for user input
 plt.show()
 
+
+# +
 # Import custom code and plot the figure.
+
+from custom_code import plot_distribution
 # Save this figure in the figures directory.
-path = '../figures/dist.png'
+path = '../../figures/dist.pdf'
 plot_distribution(100, 15, path)
+
