0df5d80fd4a82b4ec677fb2f6b474cca8de173af
llorracc <Christopher.Carroll@jhu.edu>
2021-04-07 23:48:09 -0400

    Further minor weaks

--- code/python/REMARK-starter-example.py
+++ code/python/REMARK-starter-example.py
@@ -1,58 +1,27 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: py,ipynb
-#     notebook_metadata_filter: all
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.2
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-#   language_info:
-#     codemirror_mode:
-#       name: ipython
-#       version: 3
-#     file_extension: .py
-#     mimetype: text/x-python
-#     name: python
-#     nbconvert_exporter: python
-#     pygments_lexer: ipython3
-#     version: 3.8.8
-#   latex_envs:
-#     LaTeX_envs_menu_present: true
-#     autoclose: false
-#     autocomplete: true
-#     bibliofile: biblio.bib
-#     cite_by: apalike
-#     current_citInitial: 1
-#     eqLabelWithNumbers: true
-#     eqNumInitial: 1
-#     hotkeys:
-#       equation: Ctrl-E
-#       itemize: Ctrl-I
-#     labels_anchors: false
-#     latex_user_defs: false
-#     report_style_numbering: false
-#     user_envs_cfg: false
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
-# +
+# In[1]:
+
+
 
 # ## A Sample Notebook in a Sample REMARK
 # 
 # An example notebook which has some basic content which a REMARK may include.
 # 
 # The REMARK notebook shouldn't just contain the code, use the markdown cells to explain the code and models.
-# -
+
+
+# In[2]:
+
 
 # required imports
 from HARK.ConsumptionSaving.ConsPrefShockModel import KinkyPrefConsumerType
 import numpy as np
 import matplotlib.pyplot as plt
+
+
+# In[3]:
 
 
 # Make and solve a preference shock consumer
@@ -61,9 +30,15 @@ PrefShockExample.cycles = 0  # Infinite horizon
 PrefShockExample.solve()
 
 
+# In[4]:
+
+
 # Unpack the consumption function for easier access.
 PrefShockExample.unpack('cFunc')
 PrefShockExample.unpack('mNrmMin')
+
+
+# In[5]:
 
 
 # Plot the consumption function at each discrete shock
@@ -79,7 +54,9 @@ plt.ion() # Necessary so that when executed from the command line it does not wa
 plt.show()
 
 
-# +
+# In[6]:
+
+
 # Import custom code and plot the figure.
 
 from custom_code import plot_distribution
