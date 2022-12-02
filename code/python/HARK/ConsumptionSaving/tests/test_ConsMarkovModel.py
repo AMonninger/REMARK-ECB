eb175bcaba09ffb6a4ed72d4ff12afe7213dd6b6
wdu9 <71188956+wdu9@users.noreply.github.com>
2022-12-02 02:23:20 -0600

    add local HARK folder

---
+++ code/python/HARK/ConsumptionSaving/tests/test_ConsMarkovModel.py
@@ -0,0 +1,197 @@
+import numpy as np
+from HARK.ConsumptionSaving.ConsIndShockModel import init_idiosyncratic_shocks
+from HARK.ConsumptionSaving.ConsMarkovModel import MarkovConsumerType
+from HARK.distribution import DiscreteDistribution, MeanOneLogNormal,combine_indep_dstns
+from copy import copy
+import unittest
+
+
+
+
+class test_ConsMarkovSolver(unittest.TestCase):
+    def setUp(self):
+
+        # Define the Markov transition matrix for serially correlated unemployment
+        unemp_length = 5  # Averange length of unemployment spell
+        urate_good = 0.05  # Unemployment rate when economy is in good state
+        urate_bad = 0.12  # Unemployment rate when economy is in bad state
+        bust_prob = 0.01  # Probability of economy switching from good to bad
+        recession_length = 20  # Averange length of bad state
+        p_reemploy = 1.0 / unemp_length
+        p_unemploy_good = p_reemploy * urate_good / (1 - urate_good)
+        p_unemploy_bad = p_reemploy * urate_bad / (1 - urate_bad)
+        boom_prob = 1.0 / recession_length
+        MrkvArray = np.array(
+            [
+                [
+                    (1 - p_unemploy_good) * (1 - bust_prob),
+                    p_unemploy_good * (1 - bust_prob),
+                    (1 - p_unemploy_good) * bust_prob,
+                    p_unemploy_good * bust_prob,
+                ],
+                [
+                    p_reemploy * (1 - bust_prob),
+                    (1 - p_reemploy) * (1 - bust_prob),
+                    p_reemploy * bust_prob,
+                    (1 - p_reemploy) * bust_prob,
+                ],
+                [
+                    (1 - p_unemploy_bad) * boom_prob,
+                    p_unemploy_bad * boom_prob,
+                    (1 - p_unemploy_bad) * (1 - boom_prob),
+                    p_unemploy_bad * (1 - boom_prob),
+                ],
+                [
+                    p_reemploy * boom_prob,
+                    (1 - p_reemploy) * boom_prob,
+                    p_reemploy * (1 - boom_prob),
+                    (1 - p_reemploy) * (1 - boom_prob),
+                ],
+            ]
+        )
+
+        init_serial_unemployment = copy(init_idiosyncratic_shocks)
+        init_serial_unemployment["MrkvArray"] = [MrkvArray]
+        init_serial_unemployment[
+            "UnempPrb"
+        ] = 0.0  # to make income distribution when employed
+        init_serial_unemployment["global_markov"] = False
+        self.model = MarkovConsumerType(**init_serial_unemployment)
+        self.model.cycles = 0
+        self.model.vFuncBool = False  # for easy toggling here
+
+        # Replace the default (lognormal) income distribution with a custom one
+        employed_income_dist = DiscreteDistribution(
+            np.ones(1), np.array([[1.0],[1.0]])
+        )  # Definitely get income
+        unemployed_income_dist = DiscreteDistribution(
+            np.ones(1), np.array([[1.0],[0.0]])
+        )  # Definitely don't
+        self.model.IncShkDstn = [
+            [
+                employed_income_dist,
+                unemployed_income_dist,
+                employed_income_dist,
+                unemployed_income_dist,
+            ]
+        ]
+
+    def test_check_markov_inputs(self):
+        # check Rfree
+        self.assertRaises(ValueError, self.model.check_markov_inputs)
+        # fix Rfree
+        self.model.Rfree = np.array(4 * [self.model.Rfree])
+        # check MrkvArray, first mess up the setup
+        self.MrkvArray = self.model.MrkvArray
+        self.model.MrkvArray = np.random.rand(3, 3)
+        self.assertRaises(ValueError, self.model.check_markov_inputs)
+        # then fix it back
+        self.model.MrkvArray = self.MrkvArray
+        # check LivPrb
+        self.assertRaises(ValueError, self.model.check_markov_inputs)
+        # fix LivPrb
+        self.model.LivPrb = [np.array(4 * self.model.LivPrb)]
+        # check PermGroFac
+        self.assertRaises(ValueError, self.model.check_markov_inputs)
+        # fix PermGroFac
+        self.model.PermGroFac = [np.array(4 * self.model.PermGroFac)]
+
+    def test_solve(self):
+        self.model.Rfree = np.array(4 * [self.model.Rfree])
+        self.model.LivPrb = [np.array(4 * self.model.LivPrb)]
+        self.model.PermGroFac = [np.array(4 * self.model.PermGroFac)]
+        self.model.solve()
+
+    def test_simulation(self):
+        self.model.Rfree = np.array(4 * [self.model.Rfree])
+        self.model.LivPrb = [np.array(4 * self.model.LivPrb)]
+        self.model.PermGroFac = [np.array(4 * self.model.PermGroFac)]
+        self.model.solve()
+        self.model.T_sim = 120
+        self.model.MrkvPrbsInit = [0.25, 0.25, 0.25, 0.25]
+        self.model.track_vars = ["mNrm", 'cNrm']
+        self.model.make_shock_history()  # This is optional
+        self.model.initialize_sim()
+        self.model.simulate()
+        
+        
+    
+Markov_Dict={
+    # Parameters shared with the perfect foresight model
+
+
+    "CRRA":2,                            # Coefficient of relative risk aversion
+    "Rfree": np.array([1.05**.25 ]* 2),      # Interest factor on assets
+    "DiscFac":  .985,                    # Intertemporal discount factor
+    "LivPrb" : [np.array([.99375]*2)],                # Survival probability
+    "PermGroFac" : [np.array([1.00]*2)],               # Permanent income growth factor
+
+    # Parameters that specify the income distribution over the lifecycle
+    "PermShkStd" : [.06],                  # Standard deviation of log permanent shocks to income
+    "PermShkCount" : 7,                    # Number of points in discrete approximation to permanent income shocks
+    "TranShkStd" : [.3],                   # Standard deviation of log transitory shocks to income
+    "TranShkCount" : 7,                    # Number of points in discrete approximation to transitory income shocks
+    "UnempPrb" : 0.00, #.08                # Probability of unemployment while working
+    "IncUnemp" :  0.0,                     # Unemployment benefits replacement rate
+    "UnempPrbRet" : 0.0005,                # Probability of "unemployment" while retired
+    "IncUnempRet" : 0.0,                   # "Unemployment" benefits when retired
+    "T_retire" : 0.0,                      # Period of retirement (0 --> no retirement)
+    "tax_rate" : 0.0,                      # Flat income tax rate (legacy parameter, will be removed in future)
+
+    # Parameters for constructing the "assets above minimum" grid
+    "aXtraMin" : 0.001,                    # Minimum end-of-period "assets above minimum" value
+    "aXtraMax" : 60,                       # Maximum end-of-period "assets above minimum" value
+    "aXtraCount" : 60,                     # Number of points in the base grid of "assets above minimum"
+    "aXtraNestFac" : 4,                    # Exponential nesting factor when constructing "assets above minimum" grid
+    "aXtraExtra" : [None],                 # Additional values to add to aXtraGrid
+
+    # A few other parameters
+    "BoroCnstArt" : 0.0,                   # Artificial borrowing constraint; imposed minimum level of end-of period assets
+    "vFuncBool" : True,                    # Whether to calculate the value function during solution
+    "CubicBool" : False,                   # Preference shocks currently only compatible with linear cFunc
+    "T_cycle" : 1,                         # Number of periods in the cycle for this agent type
+
+    # Parameters only used in simulation
+    "AgentCount" : 100000,                 # Number of agents of this type
+    "T_sim" : 200,                         # Number of periods to simulate
+    "aNrmInitMean" : np.log(.8)-(.5**2)/2, # Mean of log initial assets
+    "aNrmInitStd"  : .5,                   # Standard deviation of log initial assets
+    "pLvlInitMean" : 0.0,                  # Mean of log initial permanent income
+    "pLvlInitStd"  : 0.0,                  # Standard deviation of log initial permanent income
+    "PermGroFacAgg" : 1.0,                 # Aggregate permanent income growth factor
+    "T_age" : None,                        # Age after which simulated agents are automatically killed
+    
+    # markov array
+    "MrkvArray"   :  [ np.array( [ [0.984 ,  0.856 ] ,  
+                          
+                               [ 0.0152 , 0.1432777460476552] ]          ).T ]
+     }
+
+    
+class test_make_EndOfPrdvFuncCond(unittest.TestCase):
+    
+    def main_test(self):
+        
+        
+        Markov_vFuncBool_example = MarkovConsumerType(**Markov_Dict)
+        
+        TranShkDstn_e = MeanOneLogNormal(Markov_vFuncBool_example.TranShkStd[0],123).approx(Markov_vFuncBool_example.TranShkCount)
+        TranShkDstn_u = DiscreteDistribution( np.ones(1),  np.ones(1)*.2 )
+        PermShkDstn = MeanOneLogNormal(Markov_vFuncBool_example.PermShkStd[0],123).approx(Markov_vFuncBool_example.PermShkCount)
+        
+        #employed Income shock distribution
+        employed_IncShkDstn = combine_indep_dstns(PermShkDstn,TranShkDstn_e)
+        
+        #unemployed Income shock distribution
+        unemployed_IncShkDstn = combine_indep_dstns(PermShkDstn,TranShkDstn_u)
+        
+        # Specify list of IncShkDstns for each state
+        Markov_vFuncBool_example.IncShkDstn = [ [employed_IncShkDstn, unemployed_IncShkDstn] ]
+        
+        #solve the consumer's problem
+        Markov_vFuncBool_example.solve()
+
+        self.assertAlmostEqual(Markov_vFuncBool_example.solution[0].vFunc[1](0.4),  -4.127935542867632 )
+
+    
+    
---
+++ code/python/HARK/tests/test_distribution.py
@@ -0,0 +1,326 @@
+import unittest
+
+import numpy as np
+
+from HARK.distribution import (
+    Bernoulli,
+    calc_expectation,
+    distr_of_function,
+    calc_lognormal_style_pars_from_normal_pars,
+    calc_normal_style_pars_from_lognormal_pars,
+    combine_indep_dstns,
+    DiscreteDistribution,
+    IndexDistribution,
+    Lognormal,
+    MarkovProcess,
+    MeanOneLogNormal,
+    MVNormal,
+    Normal,
+    Uniform,
+    Weibull,
+)
+
+
+class DiscreteDistributionTests(unittest.TestCase):
+    """
+    Tests for distribution.py sampling distributions
+    with default seed.
+    """
+
+    def test_draw(self):
+        self.assertEqual(
+            DiscreteDistribution(np.ones(1), np.zeros(1)).draw(1)[0],
+            0,
+        )
+
+    def test_distr_of_function(self):
+
+        # Function 1 -> 1
+        # Approximate the lognormal expectation
+        sig = 0.05
+        norm = Normal(mu=-(sig ** 2) / 2, sigma=sig).approx(131)
+        my_logn = distr_of_function(norm, func=lambda x: np.exp(x))
+        exp = calc_expectation(my_logn)
+        self.assertAlmostEqual(exp, 1.0)
+
+        # Function 1 -> n
+        # Mean and variance of the normal
+        norm = Normal(mu=0.0, sigma=1.0).approx(5)
+        moments = distr_of_function(norm, lambda x: np.array([x, x ** 2]))
+        exp = calc_expectation(moments).flatten()
+        self.assertAlmostEqual(exp[0], 0.0)
+        self.assertAlmostEqual(exp[1], 1.0)
+
+        # Function n -> 1
+        # Expectation of the sum of two independent normals
+        mu_a, mu_b = 1.0, 2.0
+        si_a, si_b = 3.0, 4.0
+        norm_a = Normal(mu=mu_a, sigma=si_a).approx(5)
+        norm_b = Normal(mu=mu_b, sigma=si_b).approx(5)
+        binorm = combine_indep_dstns(norm_a, norm_b)
+        mysum = distr_of_function(binorm, lambda x: np.sum(x))
+        exp = calc_expectation(mysum)
+        self.assertAlmostEqual(exp[0], mu_a + mu_b)
+
+        # Function n -> m
+        # Mean and variance of two normals
+        moments = distr_of_function(
+            binorm,
+            lambda x: np.array([x[0], (x[0] - mu_a) ** 2, x[1], (x[1] - mu_b) ** 2]),
+        )
+        exp = calc_expectation(moments)
+        self.assertAlmostEqual(exp[0], mu_a)
+        self.assertAlmostEqual(exp[1], si_a ** 2)
+        self.assertAlmostEqual(exp[2], mu_b)
+        self.assertAlmostEqual(exp[3], si_b ** 2)
+
+    def test_calc_expectation(self):
+        dd_0_1_20 = Normal().approx(20)
+        dd_1_1_40 = Normal(mu=1).approx(40)
+        dd_10_10_100 = Normal(mu=10, sigma=10).approx(100)
+
+        ce1 = calc_expectation(dd_0_1_20)
+        ce2 = calc_expectation(dd_1_1_40)
+        ce3 = calc_expectation(dd_10_10_100)
+
+        self.assertAlmostEqual(ce1[0], 0.0)
+        self.assertAlmostEqual(ce2[0], 1.0)
+        self.assertAlmostEqual(ce3[0], 10.0)
+
+        ce4 = calc_expectation(dd_0_1_20, lambda x: 2 ** x)
+
+        self.assertAlmostEqual(ce4[0], 1.27153712)
+
+        ce5 = calc_expectation(dd_1_1_40, lambda x: 2 * x)
+
+        self.assertAlmostEqual(ce5[0], 2.0)
+
+        ce6 = calc_expectation(dd_10_10_100, lambda x, y: 2 * x + y, 20)
+
+        self.assertAlmostEqual(ce6[0], 40.0)
+
+        ce7 = calc_expectation(
+            dd_0_1_20, lambda x, y: x + y, np.hstack(np.array([0, 1, 2, 3, 4, 5]))
+        )
+
+        self.assertAlmostEqual(ce7.flat[3], 3.0)
+
+        PermShkDstn = MeanOneLogNormal().approx(200)
+        TranShkDstn = MeanOneLogNormal().approx(200)
+        IncShkDstn = combine_indep_dstns(PermShkDstn, TranShkDstn)
+
+        ce8 = calc_expectation(IncShkDstn, lambda X: X[0] + X[1])
+
+        self.assertAlmostEqual(ce8, 2.0)
+
+        ce9 = calc_expectation(
+            IncShkDstn,
+            lambda X, a, r: r / X[0] * a + X[1],
+            np.array([0, 1, 2, 3, 4, 5]),  # an aNrmNow grid?
+            1.05,  # an interest rate?
+        )
+
+        self.assertAlmostEqual(ce9[3], 9.518015322143837)
+
+
+class MatrixDiscreteDistributionTests(unittest.TestCase):
+    """
+    Tests matrix-valued discrete distribution.
+    """
+
+    def setUp(self):
+
+        self.draw_1 = np.array(
+            [[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]], [[7.0, 8.0, 9.0], [10.0, 11.0, 12.0]],]
+        )
+
+        self.draw_2 = -1 * self.draw_1
+
+        X = np.stack([self.draw_1, self.draw_2], axis=-1)
+        pmf = np.array([0.5, 0.5])
+
+        self.mat_distr = DiscreteDistribution(pmf, X, seed=0)
+
+    def test_draw(self):
+        """
+        Check that the draws are the matrices we
+        want them to be
+        """
+
+        draw = self.mat_distr.draw(1)
+        self.assertTrue(np.allclose(draw[..., 0], self.draw_2))
+
+    def test_expected(self):
+
+        # Expectation without transformation
+        exp = calc_expectation(self.mat_distr)
+
+        # Check the expectation is of the shape we want
+        self.assertTrue(exp.shape[0] == self.draw_1.shape[0])
+        self.assertTrue(exp.shape[1] == self.draw_1.shape[1])
+
+        # Check that its value is what we expect
+        self.assertTrue(np.allclose(exp, 0.0))
+
+        # Expectation of the sum
+        exp = calc_expectation(self.mat_distr, func=np.sum)
+        self.assertTrue(float(exp) == 0.0)
+
+    def test_distr_of_fun(self):
+
+        # A function that receives a (2,n,m) matrix
+        # and sums across n, getting a (2,1,m) matrix
+        def myfunc(mat):
+
+            return np.sum(mat, axis=1, keepdims=True)
+
+        mydistr = distr_of_function(self.mat_distr, myfunc)
+
+        # Check the dimensions
+        self.assertTrue(mydistr.dim() == (2, 1, 3))
+
+
+class DistributionClassTests(unittest.TestCase):
+    """
+    Tests for distribution.py sampling distributions
+    with default seed.
+    """
+
+    def test_drawMeanOneLognormal(self):
+        self.assertEqual(MeanOneLogNormal().draw(1)[0], 3.5397367004222002)
+
+    def test_Lognormal(self):
+        dist = Lognormal()
+
+        self.assertEqual(dist.draw(1)[0], 5.836039190663969)
+
+        dist.draw(100)
+        dist.reset()
+
+        self.assertEqual(dist.draw(1)[0], 5.836039190663969)
+
+    def test_Normal(self):
+        dist = Normal()
+
+        self.assertEqual(dist.draw(1)[0], 1.764052345967664)
+
+        dist.draw(100)
+        dist.reset()
+
+        self.assertEqual(dist.draw(1)[0], 1.764052345967664)
+
+    def test_MVNormal(self):
+        dist = MVNormal()
+
+        self.assertTrue(
+            np.allclose(dist.draw(1)[0], np.array([2.76405235, 1.40015721]))
+        )
+
+        dist.draw(100)
+        dist.reset()
+
+        self.assertTrue(
+            np.allclose(dist.draw(1)[0], np.array([2.76405235, 1.40015721]))
+        )
+
+    def test_Weibull(self):
+        self.assertAlmostEqual(Weibull().draw(1)[0], 0.79587450816311)
+
+    def test_Uniform(self):
+        uni = Uniform()
+
+        self.assertEqual(Uniform().draw(1)[0], 0.5488135039273248)
+
+        self.assertEqual(calc_expectation(uni.approx(10)), 0.5)
+
+    def test_Bernoulli(self):
+        self.assertEqual(Bernoulli().draw(1)[0], False)
+
+
+class IndexDistributionClassTests(unittest.TestCase):
+    """
+    Tests for distribution.py sampling distributions
+    with default seed.
+    """
+
+    def test_IndexDistribution(self):
+        cd = IndexDistribution(Bernoulli, {"p": [0.01, 0.5, 0.99]})
+
+        conditions = np.array([0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2])
+
+        draws = cd.draw(conditions)
+
+        self.assertEqual(draws[:4].sum(), 0)
+        self.assertEqual(draws[-4:].sum(), 4)
+        self.assertEqual(cd[2].p.tolist(), 0.99)
+
+    def test_IndexDistribution_approx(self):
+        cd = IndexDistribution(
+            Lognormal, {"mu": [0.01, 0.5, 0.99], "sigma": [0.05, 0.05, 0.05]}
+        )
+
+        approx = cd.approx(10)
+
+        draw = approx[2].draw(5)
+
+        self.assertAlmostEqual(draw[1], 2.93868620)
+
+    def test_IndexDistribution_seeds(self):
+        cd = IndexDistribution(Lognormal, {"mu": [1, 1], "sigma": [1, 1]})
+
+        draw_0 = cd[0].draw(1).tolist()
+        draw_1 = cd[1].draw(1).tolist()
+
+        self.assertNotEqual(draw_0, draw_1)
+
+
+class MarkovProcessTests(unittest.TestCase):
+    """
+    Tests for MarkovProcess class.
+    """
+
+    def test_draw(self):
+        mrkv_array = np.array([[0.75, 0.25], [0.1, 0.9]])
+
+        mp = MarkovProcess(mrkv_array)
+
+        new_state = mp.draw(np.zeros(100).astype(int))
+
+        self.assertEqual(new_state.sum(), 20)
+
+        new_state = mp.draw(new_state)
+
+        self.assertEqual(new_state.sum(), 39)
+
+
+class LogNormalToNormalTests(unittest.TestCase):
+    """
+    Tests methods to convert between lognormal and normal parameters.
+    """
+
+    def test_lognorm_to_norm(self):
+        avg_ln, std_ln = 1.0, 0.2
+        avg_n, std_n = calc_normal_style_pars_from_lognormal_pars(avg_ln, std_ln)
+        avg_hat, std_hat = calc_lognormal_style_pars_from_normal_pars(avg_n, std_n)
+
+        self.assertAlmostEqual(avg_ln, avg_hat)
+        self.assertAlmostEqual(std_ln, std_hat)
+
+    def test_norm_to_lognorm(self):
+        avg_n, std_n = 1.0, 0.2
+        avg_ln, std_ln = calc_lognormal_style_pars_from_normal_pars(avg_n, std_n)
+        avg_hat, std_hat = calc_normal_style_pars_from_lognormal_pars(avg_ln, std_ln)
+
+        self.assertAlmostEqual(avg_n, avg_hat)
+        self.assertAlmostEqual(std_n, std_hat)
+
+
+class NormalDistTest(unittest.TestCase):
+    def test_approx_equiprobable(self):
+
+        mu, sigma = 5.0, 27.0
+
+        points = Normal(mu, sigma).approx_equiprobable(701).X
+
+        self.assertAlmostEqual(np.mean(points), mu, places=7)
+        self.assertAlmostEqual(np.std(points), sigma, places=2)
